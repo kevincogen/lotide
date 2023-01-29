b64c7adf7701cb3552948000c388336fc516985f
Kevin Cogen <k.cogen@gmail.com>
2023-01-29 18:32:32 -0500

    compares two arrays and returns true if Arrays are equal and false if Arrays are not equal

--- eqArrays.js
+++ eqArrays.js
@@ -26,7 +26,7 @@ const eqArrays = function(arrayOne, arrayTwo) {
 };
 
 
-
+// Testing with assertEqual function
 assertEqual(eqArrays([1, 2, 3, 4, 5], [1, 2, 3]), false); // => should PASS
 assertEqual(eqArrays([1, 2, 3, 4, 5], [1, 2, 3]), true); // => should FAIL
 assertEqual(eqArrays([1, 2, 3, 4, 5], [1, 2, 3, 4, 5]), false); // => should FAIL
