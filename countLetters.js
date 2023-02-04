660d70a9f5d877fbe16aa5b3d0ad2e3b81f91fa7
Kevin Cogen <k.cogen@gmail.com>
2023-02-04 17:58:57 -0500

    Input a string, return object that counts each letter used. Only removes spaces at this time, not functional to filter all non-alphanumeric values from count

---
+++ countLetters.js
@@ -0,0 +1,18 @@
+const countLetters = function(sentence) {
+
+  let letterCount = {}; //object to return
+  //for Loop goes through each character and adds value = 1 if char not present in object yet.
+  //if char is present as a property, it adds 1 to the value. yay!
+  for (let char of sentence) {
+    if (letterCount[char]) {
+      letterCount[char] += 1;
+    } else {
+      letterCount[char] = 1;
+    }
+  }
+  delete letterCount[" "]; //this is not scaled to remove everything not alphanumeric, only removes spaces.
+  return letterCount;
+};
+
+//Log test
+console.log(countLetters("lighthouse in the house"));
