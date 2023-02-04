40bb3a9c70c2ef628753ca31d76a444c7c180e47
Kevin Cogen <k.cogen@gmail.com>
2023-02-04 18:39:07 -0500

    input string, return object with list of letters used, and their index position in the string. This only removes spaces, does not filter out non-letter characters.

---
+++ letterPositions.js
@@ -0,0 +1,14 @@
+const letterPositions = function(sentence) {
+  const results = {};
+  //typical (c) for loop is better here, since we need to access index value as well as identify value at index :)
+  for (let i = 0; i < sentence.length; i++) {
+    if (results[sentence[i]]) {
+      results[sentence[i]].push(i);
+    } else {
+      results[sentence[i]] = [i];
+    }
+  };
+  delete results[" "];
+  return results;
+};
+console.log(letterPositions("lighthouse in the house"));
