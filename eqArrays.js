48d6ae0faa735dd9249c2e2ba2a269baa82403bc
Kevin Cogen <k.cogen@gmail.com>
2023-02-19 18:42:41 -0500

    refactor: added module export, changed function code to be more efficient, added module.exports

--- eqArrays.js
+++ eqArrays.js
@@ -1,35 +1,13 @@
-// Assert Equal function copy
-const assertEqual = function(actual, expected) {
-  if (actual !== expected) {
-    console.log(`🟥😅 Assertion Failed: ${actual} !== ${expected}`);
-  } else if (actual === expected) {
-    console.log(`✅😎 Assertion Passed: ${actual} === ${expected}`);
-  }
-};
-
 //eqArrays function
-
-const eqArrays = function(arrayOne, arrayTwo) {
-  if (arrayOne.length === arrayTwo.length) {
-    let truthArray = [];
-    for (let i = 0; i < arrayOne.length; i++) {
-      if (arrayOne[i] === arrayTwo[i]) {
-        truthArray.push("true");
-      } else {
-        return false;
-      }
-      if (truthArray.length === arrayOne.length) {
-        return true;
-      }
+const eqArrays = function(arrOne, arrTwo) {
+  if (arrOne.length !== arrTwo.length) {
+    return false;
+  }
+  for (let i = 0; i < arrOne.length; i++) {
+    if (arrOne[i] !== arrTwo[i]) {
+      return false;
     }
-  } else return false;
+  } return true; 
 };
 
-
-// Testing with assertEqual function
-assertEqual(eqArrays([1, 2, 3, 4, 5], [1, 2, 3]), false); // => should PASS
-assertEqual(eqArrays([1, 2, 3, 4, 5], [1, 2, 3]), true); // => should FAIL
-assertEqual(eqArrays([1, 2, 3, 4, 5], [1, 2, 3, 4, 5]), false); // => should FAIL
-assertEqual(eqArrays([1, 2, 3, 4, 5], [1, 2, 3, 4, 5]), true); // => should FAIL
-assertEqual(eqArrays(["Hey", "This", "Should", "Pass", "Kevin"], ["Hey", "This", "Should", "Pass", "Kevin"]), true); // => should pass
-assertEqual(eqArrays(["Hey", "This", "Should ", "Pass", "Kevin"], ["Hey", "This", "Should", "Pass", "Kevin"]), true); // => should fail
+module.exports = eqArrays;
