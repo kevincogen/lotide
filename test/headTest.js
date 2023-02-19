96527e1e14f203017a91a8fd1baeeca9d419e308
Kevin Cogen <k.cogen@gmail.com>
2023-02-19 18:23:22 -0500

    Tests for Head function

---
+++ test/headTest.js
@@ -0,0 +1,19 @@
+const assertEqual = require('./assertEqual');
+
+//Function head returns the first item in the array
+const head = function(array) {
+  return array[0]
+}
+
+//Test cases
+
+//returns 'three'
+console.log(head(['three', 2, 3, '5']))
+
+//asserts equal using only the head of the array 
+
+//returns Assertion Passed
+console.log(assertEqual(head([5,6,7]), 5))
+
+//returns Assertion Passed
+console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"))
