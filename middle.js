c12ba0eb5a7ffed7c7622f09c831e97443dece21
Kevin Cogen <k.cogen@gmail.com>
2023-02-19 19:16:34 -0500

    refactor - added module export, refactor - code more efficient

--- middle.js
+++ middle.js
@@ -1,25 +1,12 @@
-
 //Middle Function
-const middle = function(inputArray) {
-  let middleArray = [];
-
-  if (inputArray.length < 3) {
-    // less than 3: return empty array
-    middleArray = [];
-  } else if (inputArray.length > 2 && inputArray.length % 2 === 0) {
-    // even: divide by array length -1 and output round down math.floor and rounding up math.ceil to get
-    // the two middle elements
-    middleArray.push(inputArray[Math.floor(((inputArray.length - 1) / 2))]);
-    middleArray.push(inputArray[Math.ceil(((inputArray.length - 1) / 2))]);
-    
-  } else if (inputArray.length > 2 && inputArray.length % 2 !== 0) {
-    //odd: divide by array length -1 by 2 for middle element
-    middleArray.push(inputArray[((inputArray.length - 1) / 2)]);
+const middle = function(array) {
+  if (array.length <= 2) {
+    return [];
+  } else if (array.length % 2 === 0) {
+    return [array[Math.ceil(array.length / 2) - 1], array[Math.ceil(array.length / 2)]];
+  } else {
+    return [array[Math.floor(array.length / 2)]];
   }
-  return middleArray;
 };
-//Testing Console Log
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
-console.log(middle([1, 2, 3, 4])); // => [2,3]
-console.log(middle([1, 2, 3, 4, 5, 6, 7, 8])); // => [4, 5]
+
+module.exports = middle;
