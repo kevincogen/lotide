840495eed2e07af20055bf16e40a3732e3c64301
Kevin Cogen <k.cogen@gmail.com>
2023-02-19 21:51:29 -0500

    Mocha and Chai test refactor

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,31 +1,21 @@
-const assertEqual = require('../assertEqual');
-
 // tail function returns all items of the array that occur after the first instance,
 // but not including the first instance.
 
-const tail = function(array) {
-  let tailArray = [];
+const tail = require('../tail')
 
-  for (let index of array) {
-    if (index !== array[0]) {
-      tailArray.push(index);
-    }
-  } return tailArray;
-};
+//Mocha and Chai Tests
+const assert = require('chai').assert;
 
-//Test Cases
 
-//Prints new array sans head
-console.log(tail([45,7,"hey",3,4,5]));
-
-//verifies if new array items are equal to our expected inputs
-const result = tail(["Hello", "Lighthouse","Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
-
-//array with only 1 element should yield empty array for its tail
-console.log(tail([2]));
-
-//empty array should yield an empty array for its tail
-console.log(tail([]));
+describe("#tail", () => {
+  it("returns[7,'hey',3,4,5] for [45,7,'hey',3,4,5] ", () => {
+    assert.deepEqual(tail([45,7,'hey',3,4,5]), [7,'hey',3,4,5]);
+  });
+});
+it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+  assert.deepEqual(tail(['Hello', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']); 
+});
+it("asserts length return is 2 for ['Hello', 'Lighthouse', 'Labs']", () => {
+  const result = tail(["Hello", "Lighthouse","Labs"]);
+  assert.strictEqual(result.length, 2);
+});
