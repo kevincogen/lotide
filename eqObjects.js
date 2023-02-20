a9e8c95d9c894365968883f4f784f22f773a4a21
Kevin Cogen <k.cogen@gmail.com>
2023-02-20 11:45:25 -0500

    refactor adds module export and removes tests

--- eqObjects.js
+++ eqObjects.js
@@ -1,24 +1,12 @@
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
-
-
-//eqArray declare
-const eqArrays = function(arrOne, arrTwo) {
-  if (arrOne.length !== arrTwo.length) {
-    return false;
-  }
-  for (let i = 0; i < arrOne.length; i++) {
-    if (arrOne[i] !== arrTwo[i]) {
-      return false;
-    }
-  } return true; //if we complete the loop, none are false. No need for Truth Array, lol. 
-};
-
+//eqArrays declare
+const eqArrays = require('./eqArrays');
 
 //eqObjects
 const eqObjects = function(object1, object2) {
-  let objKeysOne = Object.keys(object1)
-  let objKeysTwo = Object.keys(object2) //converts to array of keys
+  let objKeysOne = Object.keys(object1);
+  let objKeysTwo = Object.keys(object2); //converts to array of keys
 
   if (objKeysOne.length !== objKeysTwo.length) { 
     return false; //not equal length
@@ -33,44 +21,4 @@ const eqObjects = function(object1, object2) {
     }
   } return true; //If we complete the loop, none are false. 
 };
-
-//Testing: Primitive Values 
-const shirtObject = { color: "red", size: "medium" };
-const anotherShirtObject= { size: "medium", color: "red" };
-console.log(eqObjects(shirtObject , anotherShirtObject)); // => true
-
-const longSleeveShirtObject= { size: "medium", color: "red", sleeveLength: "long" };
-console.log(eqObjects(shirtObject , longSleeveShirtObject)); // => false
-
-//Testing: Arrays as Values
-const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
-const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
-console.log(eqObjects(multiColorShirtObject  , anotherMultiColorShirtObject)); // => true
-
-const longSleeveMultiColorShirtObject= { size: "medium", colors: ["red", "blue"], sleeveLength: "long" };
-console.log(eqObjects(multiColorShirtObject  , longSleeveMultiColorShirtObject)); // => false
-
-
-// Attempt 1: This flattens each Object to an array, and compares eqArray. 
-// This does not allow for object properties to be listed in a different order. 
-/*
-const eqObjects = function(object1, object2) {
-  //turn into arrays
-  let objArrOne = Object.entries(object1)
-  let objArrTwo = Object.entries(object2)
-  
-  if (objArrOne.length === objArrTwo.length) {
-    let truthArray = [];
-    for (let i = 0; i < objArrOne.length; i++) {
-      if (objArrOne[i] === objArrTwo[i]) {
-        truthArray.push("true");
-      } else {
-        return false; //Do i need this?
-      }
-      if (truthArray.length === objArrOne.length) {
-        return true;
-      }
-    }
-  } else return false;
-}; 
-*/
+module.exports = eqObjects;
