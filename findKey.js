10b56eef0ac5793824424957e56adfdb3c7e98d0
Kevin Cogen <k.cogen@gmail.com>
2023-02-20 11:52:07 -0500

    removed test, added module export

--- findKey.js
+++ findKey.js
@@ -6,12 +6,4 @@ const findKey = function(object, callback) {
   }
 };
 
-//example
-console.log(findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 2)); // => "noma"
+module.exports = findKey;
